96899a5dbd27f3553a1652efb614f3b9af447539
skyfoxx <skyfoxx@sacred.finance>
2024-11-17 16:58:36 +1100

    chore: cleanup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import { geistSans, geistMono } from '@/lib/fonts';
+
 export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
                 <Main />
                 <NextScript />
             </body>
