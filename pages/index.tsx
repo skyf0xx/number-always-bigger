b5597359c62ae952d93a51fbc8b3576ae6b297fa
skyfoxx <skyfoxx@sacred.finance>
2024-11-25 15:56:24 +1100

    fix: scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,8 @@ export default function Home() {
     const handleWalletConnection = () => {
         if (!connected) {
             connect();
+        } else {
+            scrollToDashboard();
         }
     };
 
