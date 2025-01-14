9ffd22f61a5c04b66ec45ba5a497b6681ed245d7
skyfoxx <skyfoxx@sacred.finance>
2025-01-14 17:47:54 +1100

    feat: update twitter

--- pages/index.tsx
+++ pages/index.tsx
@@ -240,10 +240,10 @@ export default function Home() {
                     <div className="flex justify-center gap-8 mb-8">
                         <a
                             target="_blank"
-                            href="https://x.com/AlwaysBigger"
+                            href="https://x.com/Mithril_Labs"
                             className="bg-white px-6 py-3 rounded-full font-bold transform hover:scale-110 transition-transform hover:-rotate-2 border-2 border-black"
                         >
-                            Twitter: @AlwaysBigger
+                            Twitter: @Mithril_Labs
                         </a>
                         <a
                             target="_blank"
